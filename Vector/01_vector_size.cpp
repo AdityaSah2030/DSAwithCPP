e9f2d86cdea640ff094569b293339150aea0fa37
Aditya Sah <adityasah2030@gmail.com>
2025-03-12 12:33:17 +0530

    Update 01_vector_size.cpp

--- Vector/01_vector_size.cpp
+++ Vector/01_vector_size.cpp
@@ -3,23 +3,30 @@
 using namespace std;
 
 /*
- * Demonstrates how to get the size of a vector using size().
- * 
- * Instructions to compile and run (Linux/Mac):
- * 1. Compile:  g++ 01_vector_size.cpp -o 01_vector_size
- * 2. Run:      ./01_vector_size
+ * Program: 01_vector_size.cpp
+ * Description:
+ *   Demonstrates how to get the size of a vector using size().
  *
- * Instructions to compile and run (Windows):
- * 1. Compile:  g++ 01_vector_size.cpp -o 01_vector_size.exe
- * 2. Run:      01_vector_size
+ * Expected Console Output:
+ *   The size of the vector is: 5
+ *
+ * Steps to Compile and Run:
+ *   Linux/Mac:
+ *     g++ 01_vector_size.cpp -o 01_vector_size
+ *     ./01_vector_size
+ *   Windows:
+ *     g++ 01_vector_size.cpp -o 01_vector_size.exe
+ *     01_vector_size
  */
 
 int main() {
-    // Create a vector with some integer elements
-    vector<int> v {1, 2, 3, 4, 5};
+    // Create a vector with 5 integer elements
+    vector<int> v {1, 2, 3, 4, 5};  
+    // Vector state: [1, 2, 3, 4, 5]
 
-    // Print the size of the vector
+    // Output the size of the vector
     cout << "The size of the vector is: " << v.size() << endl;
+    // Expected output: The size of the vector is: 5
 
     return 0;
 }
